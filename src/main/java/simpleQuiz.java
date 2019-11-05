5c029816adc7174311dfac13c60b542631467796
Tomek <tomm.wrobel@gmail.com>
2019-11-05 23:44:15 +0100

    Simple game

--- src/main/java/simpleQuiz.java
+++ src/main/java/simpleQuiz.java
@@ -1,12 +1,38 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class simpleQuiz {
 
     public static void main(String[] args)
     {
+        Random rNum = new Random();
+        Boolean win = false;
+        int Result = 0;
+        int qNum = rNum.nextInt(100) + 1;
         Scanner scan = new Scanner(System.in);
-        System.out.print("Jak masz na imię?: ");
-        String name = scan.next();
-        System.out.println("Witaj " + name + "!");
+
+        do {
+
+            System.out.println("Zgadnij liczbę z zakresu 1-100: ");
+            int pNum = Integer.parseInt(scan.next());
+
+            if(pNum == qNum)
+            {
+                win = true;
+                Result++;
+                System.out.println("Wygrales! Twój wynik to " + Result);
+            }
+            else if(pNum < qNum)
+            {
+                Result++;
+                System.out.println("Podałeś za małą liczbę!");
+            }
+            else
+            {
+                Result++;
+                System.out.println("Podałeś za dużą liczbę!");
+            }
+
+        } while (win == false);
     }
 }
